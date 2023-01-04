360222aa7a02dd8e3848e24044d7fd9b3c4b2f6a
Jorge <jmarquezrave@gmail.com>
2023-01-04 16:28:54 -0300

    removed sample function from v2.php

--- api/apiology/version/v2.php
+++ api/apiology/version/v2.php
@@ -15,31 +15,4 @@ class V2
     {
         return false;
     }
-
-    public function sample($_data)
-    {
-        $this->http_response = new HTTP();
-        // temp variable "$function" remove in production
-        // true => simple algorithm inside the function with a http message in return
-        // false => calls module -> class -> function
-        $function = false;
-        if ($function) {
-
-            $this->status_code = 200;
-            $this->response['message'] = "Welcome to the version => 2, resource => sample";
-
-            $this->http_response->httpJsonResponse(
-                $this->status_code,
-                $this->response
-            );
-        } else {
-            $file = "apiology/version/resources/sample.php";
-            if (file_exists($file)) {
-                require $file;
-                $module = "Apiology\\Apiology\\version\\resources\\Sample";
-                $module = new $module();
-                $module->sample($_data);
-            }
-        }
-    }
 }
